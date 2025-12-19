5ec3eb22c0d5b8a44a07be27d7d0f8af3ecc7882
ILYASSEHARRAK <eilyass913@gmail.com>
2025-12-20 00:03:05 +0100

    profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -37,7 +37,7 @@
                 <div>
                     <label class="block text-sm font-medium mb-1">Email</label>
                     <input type="email" name="email" value="{{ auth()->user()->email }}"
-                           class="w-full border rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500">
+                           class="w-full border rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500" readOnly>
                 </div>
 
                 <div>
